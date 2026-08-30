327076c1d9a5a210971626c59cd015c92758efb9
loreju <loreju52@gmail.com>
2026-08-30 21:34:42 +0000

    Add service worker with install and fetch listeners

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => {
+  // Activa la instalación en segundo plano
+});
+
+self.addEventListener('fetch', (e) => {
+  // Permite que la app cargue de forma instantánea
+});
