2d65d44e771518e7cf7b81c04cc60cebfa7d8a4f
loreju <loreju52@gmail.com>
2026-09-04 21:32:37 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,14 @@
+// Service Worker fantasma per activar el botó sense alentir el vídeo
+const CACHE_NAME = 'caudete-fiestas-v99';
 
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Buit. Deixa passar el streaming a màxima velocitat nativa.
+});
